8efe5f84ad3d6684d69d6c90b485d97692c69062
Cheryl Wu <grungerabbit@gmail.com>
2015-10-28 19:41:11 -0400

    focus.js 1.0.0 rc1

---
+++ focus.js
@@ -0,0 +1,18 @@
+// focus.js // version 1.0.0
+// @grungerabbit // cheryl wu
+// - - - - - - - - - - - - - - - - 
+// ? / ! / ... / $%@#& ~~> https://github.com/grungerabbit/focus.js
+
+function focusJs() {
+// {?} say console.log("no"); so hard m-t-e-f-k'rs wanna find me
+// "[Focus] means saying no to the hundred other good ideas that there are." --Steve Jobs
+  
+  setInterval(function() {
+    console.log("no");
+    focusJs();
+  }, 500);
+}
+
+// WARNING: CALL focusJs(); AT YOUR OWN RISK
+// FOCUS.JS IS LITERALLY SUPPOSED TO STACK OVERFLOW
+// focusJs();
